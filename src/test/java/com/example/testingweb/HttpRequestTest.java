eb4eab46872d5c76d85fba6811d6408984740e6c
whiplash0104 <felipe.basso.vera@gmail.com>
2022-09-07 11:45:07 -0300

    Update HttpRequestTest.java

--- src/test/java/com/example/testingweb/HttpRequestTest.java
+++ src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -22,6 +22,6 @@ public class HttpRequestTest {
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-				String.class)).contains("Hello, World");
+				String.class)).contains("Finaliza el curso DevOps Foundation v2 - Felipe Basso Vera");
 	}
 }
